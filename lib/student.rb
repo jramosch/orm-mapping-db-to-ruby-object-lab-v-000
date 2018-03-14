82a37c9736801f6aa2e976c68ef3289e0dd94a38
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-14 15:38:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 10th
+      WHERE grade = 10
       LIMIT ?
     SQL
     
